74c5f2ae1035a2ed560d24c9178b89031239ce39
arman <armanalaki@gmail.com>
2020-12-25 00:01:58 +0330

    finish create tables

--- CreateDataBase.sql
+++ CreateDataBase.sql
@@ -6,24 +6,7 @@ CREATE TABLE Country  (
     continent  nvarchar(150) ,
     PRIMARY KEY (country_id),
 );
-CREATE TABLE Player(
-    player_id INT PRIMARY KEY IDENTITY (1, 1),
-    name VARCHAR (50) NOT NULL,
-    age INT,
-	height INT,
-	weight INT,
-	shirt_number INT not null,
-	assists INT,
-	saves INT,
-	succesful_tackles INT,
-	yellow_cards INT,
-	red_cards INT,
-	position VARCHAR (20) not null,
-	team_id INT,
-	country_id INT,
-    FOREIGN KEY (team_id) REFERENCES Team(team_id),
-	FOREIGN KEY (country_id) REFERENCES Country(country_id),
-);
+
 CREATE TABLE Coach (
     coach_id int IDENTITY(1, 1) NOT NULL,
     name nvarchar(150) NOT NULL,
@@ -42,6 +25,67 @@ CREATE TABLE Commentator  (
     PRIMARY KEY (commentator_id),
 );
 
+CREATE TABLE Groups (
+	group_id int IDENTITY(1, 1) NOT NULL,
+	name char(1) ,
+	PRIMARY KEY (group_id),
+);
+
+CREATE TABLE Reffree  (
+    reffree_id int IDENTITY(1, 1) NOT NULL,
+    name nvarchar(150) NOT NULL,
+	age INT,
+	weight INT,
+	judged_matched INT,
+    PRIMARY KEY (reffree_id)
+);
+
+CREATE TABLE Stadium  (
+    stadium_id int IDENTITY(1, 1) NOT NULL,
+    name nvarchar(150) NOT NULL,
+	capacity INT,
+    city nvarchar(20),
+	country_id int not null,
+    PRIMARY KEY (stadium_id),
+	FOREIGN KEY (country_id) REFERENCES Country(country_id)
+);
+
+CREATE TABLE Team(
+    team_id INT PRIMARY KEY IDENTITY (1, 1),
+    name nVARCHAR (150) NOT NULL,
+	number_of_titles INT,
+	stablished_year date,
+	is_eliminated bit NOT null default 0,
+	score int not null default 0,
+	stadium_id INT,
+	country_id INT,
+	coach_id INT,
+	group_id INT,
+    FOREIGN KEY (stadium_id) REFERENCES Stadium(stadium_id),
+	FOREIGN KEY (country_id) REFERENCES Country(country_id),
+	FOREIGN KEY (group_id) REFERENCES Groups(group_id),
+	FOREIGN KEY (coach_id) REFERENCES Coach(coach_id)
+);
+
+CREATE TABLE Player(
+    player_id INT PRIMARY KEY IDENTITY (1, 1),
+    name VARCHAR (50) NOT NULL,
+    age INT CHECK (age>=15 and age<=50),
+	height INT,
+	weight INT,
+	shirt_number INT not null check(shirt_number>0 and shirt_number < 100),
+	assists INT,
+	saves INT,
+	succesful_tackles INT,
+	yellow_cards INT,
+	red_cards INT,
+	position VARCHAR (20) not null,
+	team_id INT,
+	country_id INT,
+    FOREIGN KEY (team_id) REFERENCES Team(team_id),
+	FOREIGN KEY (country_id) REFERENCES Country(country_id)
+);
+
 CREATE TABLE Match  (
     match_id int IDENTITY(1, 1) NOT NULL,
 	weather nvarchar(20),
@@ -53,9 +97,28 @@ CREATE TABLE Match  (
     PRIMARY KEY (match_id),
 	FOREIGN KEY (stadium_id) REFERENCES Stadium(stadium_id),
 	FOREIGN KEY (reffree_id) REFERENCES Reffree(reffree_id)
-
 );
 
+CREATE TABLE Report_by(
+    commentator_id INT not null,
+    match_id INT not null,	
+    FOREIGN KEY (commentator_id) REFERENCES Commentator(commentator_id),
+	FOREIGN KEY (match_id) REFERENCES Match(match_id)
+);
+
+CREATE TABLE Plays_in(
+    team_id INT not null,
+    match_id INT not null,	
+	offsides INT,
+    shoots INT,
+    passes INT,
+	yellow_cards INT,
+	red_cards INT,
+	foals INT,
+	is_win VARCHAR(10) not null default 'equal',
+    FOREIGN KEY (team_id) REFERENCES Team(team_id),
+	FOREIGN KEY (match_id) REFERENCES Match(match_id)
+);
 
 CREATE TABLE Goal  (
     goal_id int IDENTITY(1, 1) NOT NULL,
@@ -67,14 +130,4 @@ CREATE TABLE Goal  (
     PRIMARY KEY (goal_id),
 	FOREIGN KEY (player_id) REFERENCES Player(player_id),
 	FOREIGN KEY (match_id) REFERENCES Match(match_id)
-
 );
-
-CREATE TABLE Groups (
-	group_id int IDENTITY(1, 1) NOT NULL,
-	name char(1),
-	PRIMARY KEY (group_id),
-
-
-
-)
